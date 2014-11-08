939652af7d53b4e58e8940765e08d2772edf74c0
Ben <bahalperin@gmail.com>
2014-11-08 08:27:35 -0500

    Combined and minified files

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,48 +9,80 @@
 
 var ngrok = require('ngrok');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Load grunt tasks
-  require('load-grunt-tasks')(grunt);
+    // Load grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-  // Grunt configuration
-  grunt.initConfig({
-    pagespeed: {
-      options: {
-        nokey: true,
-        locale: "en_GB",
-        threshold: 40
-      },
-      local: {
-        options: {
-          strategy: "desktop"
+    // Grunt configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        pagespeed: {
+            options: {
+                nokey: true,
+                locale: "en_GB",
+                threshold: 40
+            },
+            local: {
+                options: {
+                    strategy: "desktop"
+                }
+            },
+            mobile: {
+                options: {
+                    strategy: "mobile"
+                }
+            }
+        },
+        concat: {
+            css: {
+                src: [
+          'css/*'
+        ],
+                dest: 'css/combined.css'
+            },
+            js: {
+                src: [
+          'js/*'
+        ],
+                dest: 'js/combined.js'
+            }
+        },
+        cssmin: {
+            css: {
+                files: {
+                    'css/style.min.css': ['css/style.css'],
+                    'css/print.min.css': ['css/print.css']
+                }
+            }
+        },
+        uglify: {
+            js: {
+                files: {
+                    'js/combined.min.js': ['js/combined.js']
+                }
+            }
         }
-      },
-      mobile: {
-        options: {
-          strategy: "mobile"
-        }
-      }
-    }
-  });
-
-  // Register customer task for ngrok
-  grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function() {
-    var done = this.async();
-    var port = 9292;
-
-    ngrok.connect(port, function(err, url) {
-      if (err !== null) {
-        grunt.fail.fatal(err);
-        return done();
-      }
-      grunt.config.set('pagespeed.options.url', url);
-      grunt.task.run('pagespeed');
-      done();
     });
-  });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // Register customer task for ngrok
+    grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function () {
+        var done = this.async();
+        var port = 9292;
 
-  // Register default tasks
-  grunt.registerTask('default', ['psi-ngrok']);
+        ngrok.connect(port, function (err, url) {
+            if (err !== null) {
+                grunt.fail.fatal(err);
+                return done();
+            }
+            grunt.config.set('pagespeed.options.url', url);
+            grunt.task.run('pagespeed');
+            done();
+        });
+    });
+
+    // Register default tasks
+    grunt.registerTask('default', ['concat:css', 'concat:js', 'uglify:js', 'psi-ngrok']);
 }
